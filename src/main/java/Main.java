57095f4d98279752682122d85174582cb5e632df
SeyyedHossein Mireslami <mireslami809@gmail.com>
2025-04-28 18:56:49 +0330

    added read map data

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,9 @@
+import models.map.FarmType;
+import models.map.Map;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Map map = new Map();
+        map.readFarmMapData(FarmType.LAKE_FARM);
     }
 }
--- src/main/java/models/map/Map.java
+++ src/main/java/models/map/Map.java
@@ -1,11 +1,34 @@
 package models.map;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+import java.io.FileReader;
+
 public class Map {
+
+
     private int width, height;
     private Tile[][] tiles;
 
-    private void readMapData() {
 
+    public void readFarmMapData(FarmType farmType) {
+        try (FileReader reader = new FileReader("src/main/resources/data/Map/farmTypes.json")) {
+            Gson gson = new Gson();
+            JsonArray jsonObject = gson.fromJson(reader, JsonArray.class);
+
+            int id = 0;
+            if (farmType == FarmType.LAKE_FARM) {
+                id = 1;
+            }
+
+            JsonObject mapObject = jsonObject.get(id).getAsJsonObject(); // Farm
+            System.out.println("width: " + width);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     private void initializeTiles() {}

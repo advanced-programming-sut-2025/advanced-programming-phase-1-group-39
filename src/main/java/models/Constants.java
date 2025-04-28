55e8f65cbbe8bd716bf570732d4650c715e0b640
SeyyedHossein Mireslami <mireslami809@gmail.com>
2025-04-28 18:40:29 +0330

    added width and height to constants

--- src/main/java/models/Constants.java
+++ src/main/java/models/Constants.java
@@ -2,4 +2,6 @@ package models;
 
 public class Constants {
     public static final int INFINITY = -1;
+    public static final int WORLD_MAP_WIDTH = 300; // 80 140 80
+    public static final int WORLD_MAP_HEIGHT = 150; // 50 50 50
 }

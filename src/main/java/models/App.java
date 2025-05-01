5be37cf13a31eb2af412160ecf6605c7d0f5c703
SeyyedHossein Mireslami <mireslami809@gmail.com>
2025-05-01 10:49:34 +0330

    App class made singleton

--- src/main/java/models/App.java
+++ src/main/java/models/App.java
@@ -3,8 +3,37 @@ package models;
 import java.util.ArrayList;
 
 public class App {
-    ArrayList<User> users;
-    ArrayList<Game> games;
-    User loggedInUser;
+    private static App app;
+    ArrayList<User> users = new ArrayList<>();
+    ArrayList<Game> games = new ArrayList<>();
+
+    User loggedInUser = null;
     boolean stayLoggedIn = false;
+
+    Game nowGame = null;
+
+    private App() {}
+
+    public static App getApp() {
+        if (app == null) {
+            app = new App();
+        }
+        return app;
+    }
+
+    public void addUser(User user) {
+        users.add(user);
+    }
+
+    public void addGame(Game game) {
+        games.add(game);
+    }
+
+    public User getLoggedInUser() {
+        return loggedInUser;
+    }
+
+    public Game getNowGame() {
+        return nowGame;
+    }
 }

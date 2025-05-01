370200502eba0b8948e0ff7ebf61ef97e38cacda
SeyyedHossein Mireslami <mireslami809@gmail.com>
2025-05-01 08:15:24 +0330

    added Game constructor

--- src/main/java/models/Game.java
+++ src/main/java/models/Game.java
@@ -5,10 +5,16 @@ import models.map.Map;
 import models.trading.TradeItem;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Game {
     ArrayList<Player> players;
-    ArrayList<TradeItem> trades;
+    ArrayList<TradeItem> trades = new ArrayList<>();
     Map gameMap;
-    ArrayList<Building> buildings;
+    ArrayList<Building> buildings = new ArrayList<>();
+
+    public Game(Player one, Player two, Player three, Player four) {
+        this.players = new ArrayList<>(List.of(one, two, three, four));
+        this.gameMap = new Map();
+    }
 }

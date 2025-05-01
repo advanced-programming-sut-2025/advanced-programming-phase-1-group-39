3323f98c012397e508df8ccabdf5dd8144087927
SeyyedHossein Mireslami <mireslami809@gmail.com>
2025-05-01 06:05:08 +0330

    dimension deleted from json

--- src/main/java/models/Constants.java
+++ src/main/java/models/Constants.java
@@ -2,6 +2,10 @@ package models;
 
 public class Constants {
     public static final int INFINITY = -1;
+
     public static final int WORLD_MAP_WIDTH = 300; // 80 140 80
     public static final int WORLD_MAP_HEIGHT = 150; // 50 50 50
+
+    public static final int FARM_WIDTH = 80;
+    public static final int FARM_HEIGHT = 50;
 }

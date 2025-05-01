a10d1247408901244b514d0ea152d3d2238a9ff2
Abolfazl <abolfazlghasemi9388@gmail.com>
2025-05-01 12:13:16 +0330

    + add method getTile

--- src/main/java/models/map/Map.java
+++ src/main/java/models/map/Map.java
@@ -8,6 +8,10 @@ public class Map {
 
     private void initializeTiles() {}
 
+    public Tile getTile(int y, int x) {
+        return tiles[y][x];
+    }
+
     public void printMap(int centerX, int centerY, int size) {}
 
     public void fillRandom(){};

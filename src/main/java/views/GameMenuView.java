bac266bc95906a46b96577a142f23ec758139a26
SeyyedHossein Mireslami <mireslami809@gmail.com>
2025-05-01 10:17:37 +0330

    added game commands and View interface for all Views note : Views have checkCommand that check if the command is exit return false to exit and save the game

--- src/main/java/views/GameMenuView.java
+++ src/main/java/views/GameMenuView.java
@@ -1,4 +1,16 @@
 package views;
 
-public class GameMenuView {
+import models.Enums.commands.GameCommands;
+
+public class GameMenuView implements View {
+
+    @Override
+    public boolean checkCommand(String command) {
+
+        if (GameCommands.PRINT_MAP.getMatcher(command) != null) {
+            System.out.println();
+        }
+
+        return true;
+    }
 }

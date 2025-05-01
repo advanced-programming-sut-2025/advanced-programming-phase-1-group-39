1e78b0e0f16fe702b8fd1ec5b25413b39ef5b120
SeyyedHossein Mireslami <mireslami809@gmail.com>
2025-05-01 12:05:36 +0330

    changed the place of fields canPlant, isWatered, isFertilized

--- src/main/java/models/map/Tile.java
+++ src/main/java/models/map/Tile.java
@@ -10,10 +10,15 @@ public class Tile {
     int x, y;
     private TileType type = TileType.SOIL;
     private ArrayList<Item> itemsOnTile = new ArrayList<>();
+
     private Plant plant = null;
     private Tree tree = null;
-
 //    private PlacedObject placedObject = null;
+
+
+    private boolean canPlant = false;
+    private boolean isWatered = false;
+    private boolean isFertilized = false;
 
     public Tile(int x, int y) {
         this.x = x;
@@ -37,5 +42,13 @@ public class Tile {
         }
         return c;
     }
+
+    public void setCanPlant() { canPlant = true; }
+    public void setIsFertilized() { isFertilized = true; }
+    public void setIsWatered() { isWatered = true; }
+
+    public boolean canPlant() { return canPlant; }
+    public boolean isFertilized() { return isFertilized; }
+    public boolean isWatered() { return isWatered; }
 }
 

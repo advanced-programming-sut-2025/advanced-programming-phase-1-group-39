53158b6ddb10c1fa52520fe7f34da5cbe0b7e1f7
SeyyedHossein Mireslami <mireslami809@gmail.com>
2025-05-01 07:45:56 +0330

    making fixed objects of map

--- src/main/java/models/map/Map.java
+++ src/main/java/models/map/Map.java
@@ -2,8 +2,10 @@ package models.map;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import models.Constants;
+import models.Enums.Direction;
 
 import java.io.FileReader;
 
@@ -12,7 +14,13 @@ public class Map {
     private Tile[][] tiles;
 
     public Map() {
-        tiles = new Tile[width][height];
+        tiles = new Tile[height][width];
+
+        for (int j = 0; j < height; j++) {
+            for (int i = 0; i < width; i++) {
+                tiles[j][i] = new Tile(i, j);
+            }
+        }
     }
 
     public String showFarmTypesInfo() {
@@ -34,7 +42,8 @@ public class Map {
         return text.toString();
     }
 
-    public void readFarmMapData(FarmType farmType) {
+    // player number is from 1 to 4
+    public void addRandomFarm(FarmType farmType, int playerNumber) {
         try (FileReader reader = new FileReader("src/main/resources/data/Map/farmTypes.json")) {
             Gson gson = new Gson();
             JsonArray array = gson.fromJson(reader, JsonArray.class);
@@ -43,18 +52,119 @@ public class Map {
             if (farmType == FarmType.LAKE_FARM) {
                 id = 1;
             }
+            JsonObject farmObject = array.get(id).getAsJsonObject(); // FarmType
 
-            JsonObject mapObject = array.get(id).getAsJsonObject(); // FarmType
+            int startX, startY;
+            switch (playerNumber) {
+                case 1:
+                    startX = Constants.WORLD_MAP_WIDTH - Constants.FARM_WIDTH;
+                    startY = 0;
+                    break;
+                case 2:
+                    startX = 0;
+                    startY = 0;
+                    break;
+                case 3:
+                    startX = 0;
+                    startY = Constants.WORLD_MAP_HEIGHT - Constants.FARM_HEIGHT;
+                    break;
+                default:
+                    startX = Constants.WORLD_MAP_WIDTH - Constants.FARM_WIDTH;
+                    startY = Constants.WORLD_MAP_HEIGHT - Constants.FARM_HEIGHT;
+                    break;
+            }
+
+            JsonObject fixedElements = farmObject.getAsJsonObject("fixedElements");
+            // adding cabin, greenhouse, main lake
+            JsonObject cabin = fixedElements.getAsJsonObject("cabin");
+            addObjectToMap(cabin, "building", startX, startY);
+
+            JsonObject greenhouse = fixedElements.getAsJsonObject("greenhouse");
+            addObjectToMap(greenhouse, "building", startX, startY);
+
+            JsonObject quarry = fixedElements.getAsJsonObject("quarry");
+            addObjectToMap(quarry, "quarry", startX, startY);
+
+            JsonArray lakes = fixedElements.getAsJsonArray("lakes");
+            JsonObject mainLake = lakes.get(0).getAsJsonObject();
+            addObjectToMap(mainLake, "lake", startX, startY);
+
+
+            // add small lake
+            if (farmType == FarmType.LAKE_FARM) {
+                JsonObject smallLake = lakes.get(1).getAsJsonObject();
+                addObjectToMap(smallLake, "lake", startX, startY);
+            }
 
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    private void initializeTiles() {}
+    private void addObjectToMap(JsonObject object, String type, int startX, int startY) {
+        int x, y, h, w;
+        x = object.get("x").getAsInt() + startX;
+        y = object.get("y").getAsInt() + startY;
+        w = object.get("w").getAsInt();
+        h = object.get("h").getAsInt();
 
-    public void printMap(int centerX, int centerY, int size) {}
+        if (type.equals("building")) {
+            for (int i = x; i < x + w; i++) {
+                tiles[y][i].setType(TileType.WALL);
+                tiles[y + h - 1][i].setType(TileType.WALL);
+            }
+            for (int j = y ; j < y + h ; j++) {
+                tiles[j][x].setType(TileType.WALL);
+                tiles[j][x + w - 1].setType(TileType.WALL);
+            }
 
-    public void fillRandom(){};
+            for (int i = x + 1; i < x + w - 1; i++) {
+                for (int j = y + 1; j < y + h - 1; j++) {
+                    tiles[j][i].setType(TileType.INDOOR);
+                }
+            }
+
+            JsonObject door = object.getAsJsonObject("door");
+            int doorX = door.get("x").getAsInt();
+            int doorY = door.get("y").getAsInt();
+            tiles[doorY][doorX].setType(TileType.INDOOR);
+        }
+        else if (type.equals("quarry")) {
+            for (int i = x; i < x + w; i++) {
+                for (int j = y; j < y + h; j++) {
+                    tiles[j][i].setType(TileType.QUARRY);
+                }
+            }
+        } else if (type.equals("lake")) {
+            for (int i = x; i < x + w; i++) {
+                for (int j = y; j < y + h; j++) {
+                    tiles[j][i].setType(TileType.WATER);
+                }
+            }
+
+            // empty tiles
+            JsonArray emptyTiles = object.getAsJsonArray("emptyTiles");
+
+            for (JsonElement element : emptyTiles) {
+                JsonObject tile = element.getAsJsonObject();
+                int tileX = tile.get("x").getAsInt();
+                int tileY = tile.get("y").getAsInt();
+
+                tiles[tileY][tileX].setType(TileType.SOIL);
+            }
+        }
+    }
+
+    public String printWholeMap() {
+        StringBuilder text = new StringBuilder();
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                text.append(tiles[i][j].getSymbol() + " ");
+            }
+            text.append("\n");
+        }
+
+        return text.toString();
+    }
 }
 
--- src/main/java/models/map/Tile.java
+++ src/main/java/models/map/Tile.java
@@ -6,10 +6,23 @@ import java.util.ArrayList;
 
 public class Tile {
     int x, y;
-    private TileType type;
-    private ArrayList<Item> itemsOnTile;
+    private TileType type = TileType.SOIL;
+    private ArrayList<Item> itemsOnTile = new ArrayList<>();
 
-    boolean isWatered;
-    boolean canPlant;
+    public Tile(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void setType(TileType type) {
+        this.type = type;
+    }
+
+    public TileType getType() {
+        return type;
+    }
+    public char getSymbol() {
+        return type.getSymbol();
+    }
 }
 

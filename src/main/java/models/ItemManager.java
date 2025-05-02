ff0227352efcc233631685a23047f6c18532858e
SeyyedHossein Mireslami <mireslami809@gmail.com>
2025-05-02 23:32:43 +0330

    - added seasons to ForagingSeed - the address of data corrected

--- src/main/java/models/ItemManager.java
+++ src/main/java/models/ItemManager.java
@@ -6,13 +6,13 @@ import models.cropsAndFarming.TreeManager;
 
 public class ItemManager {
     public static void loadItems() {
-        CropManager.loadCrops("src/main/resources/data/crops.json");
-        TreeManager.loadTrees("src/main/resources/data/trees.json");
-        ForagingManager.loadCrops("src/main/resources/data/ForagingCrops.json");
-        ForagingManager.loadSeeds("src/main/resources/data/ForagingSeeds.json");
-        ForagingManager.loadTreeSeeds("src/main/resources/data/ForagingTrees.json");
-        ForagingManager.loadMinerals("src/main/resources/data/ForagingMinerals.json");
-        ForagingManager.loadMaterials("src/main/resources/data/ForagingMaterials.json");
+        CropManager.loadCrops("src/main/resources/data/Crops/crops.json");
+        TreeManager.loadTrees("src/main/resources/data/Crops/trees.json");
+        ForagingManager.loadCrops("src/main/resources/data/Crops/ForagingCrops.json");
+        ForagingManager.loadSeeds("src/main/resources/data/Crops/ForagingSeeds.json");
+        ForagingManager.loadTreeSeeds("src/main/resources/data/Crops/ForagingTrees.json");
+        ForagingManager.loadMinerals("src/main/resources/data/Crops/ForagingMinerals.json");
+        ForagingManager.loadMaterials("src/main/resources/data/Crops/ForagingMaterials.json");
     }
 
     public static Item getItemByName(String name) {

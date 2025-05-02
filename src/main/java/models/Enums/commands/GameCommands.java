675eebb191df1f10675410cf1a4e8dba0f55a77b
SeyyedHossein Mireslami <mireslami809@gmail.com>
2025-05-02 14:06:09 +0330

    added some commands to gameCommands

--- src/main/java/models/Enums/commands/GameCommands.java
+++ src/main/java/models/Enums/commands/GameCommands.java
@@ -4,6 +4,17 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public enum GameCommands {
+    NEXT_TURN("next\\s+turn"),
+
+    TIME("time"),
+    DATE("date"),
+    DATE_AND_TIME("datetime"),
+    DAY_OF_WEEK("day\\s+of\\s+the\\s+week"),
+    CHEAT_ADVANCE_TIME("cheat\\s+advance\\s+time\\s+(?<h>-?\\d+)h"),
+    CHEAT_ADVANCE_DATE("cheat\\s+advance\\s+date\\s+(?<d>-?\\d+)d"),
+
+    SEASON("season"),
+
     PRINT_MAP("print\\s+map\\s+-l\\s+(?<x>\\d+),(?<y>\\d+)\\s+-s\\s+(?<size>\\d+)"),
     HELP_READING_MAP("help\\s+reading\\s+map");
 
--- src/main/java/views/GameView.java
+++ src/main/java/views/GameView.java
@@ -12,11 +12,28 @@ public class GameView implements View {
         GameController controller = AppControllers.gameController;
 
         Matcher matcher;
-
-        if ((matcher = GameCommands.PRINT_MAP.getMatcher(command)) != null) {
+        if ((matcher = GameCommands.NEXT_TURN.getMatcher(command)) != null) {
+            System.out.println(controller.goNextTurn());
+        } else if ((GameCommands.TIME.getMatcher(command)) != null) {
+            System.out.println(controller.showTime());
+        } else if ((GameCommands.DATE.getMatcher(command)) != null) {
+            System.out.println(controller.showDate());
+        } else if ((GameCommands.DATE_AND_TIME.getMatcher(command)) != null) {
+            System.out.println(controller.showDateTime());
+        } else if ((GameCommands.DAY_OF_WEEK.getMatcher(command)) != null) {
+            System.out.println(controller.showDayOfWeek());
+        } else if ((matcher = GameCommands.CHEAT_ADVANCE_TIME.getMatcher(command)) != null) {
+            System.out.println(controller.cheatAdvanceTime(matcher));
+        } else if ((matcher = GameCommands.CHEAT_ADVANCE_DATE.getMatcher(command)) != null) {
+            System.out.println(controller.cheatAdvanceDate(matcher));
+        } else if ((GameCommands.SEASON.getMatcher(command)) != null) {
+            System.out.println(controller.showSeason());
+        } if ((matcher = GameCommands.PRINT_MAP.getMatcher(command)) != null) {
             System.out.println(controller.printMap(matcher));
         } else if (GameCommands.HELP_READING_MAP.getMatcher(command) != null) {
             System.out.println(controller.helpReadingMap());
+        } else {
+            System.out.println("invalid command.");
         }
     }
 }

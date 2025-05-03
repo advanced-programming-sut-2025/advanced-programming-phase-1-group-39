052a070c58876d1ba07de9d4fb5d0ac66a314882
mshahmahdi <mostafashahmahdi87@gmail.com>
2025-05-03 15:47:02 +0330

    add Main MenuController function

--- src/main/java/controllers/MainMenuController.java
+++ src/main/java/controllers/MainMenuController.java
@@ -1,11 +1,31 @@
 package controllers;
 
+import models.App;
+import models.Enums.Menu;
+import models.Result;
+
 import java.util.regex.Matcher;
 
 public class MainMenuController {
-    public static void enterMenu(Matcher matcher) {}
 
-    public static void exitMenu() {}
+    public Result logout() {
+        App.getApp().setLoggedInUser(null);
+        App.getApp().setCurrentMenu(Menu.SIGNUP_MENU);
+        return new Result(true, "You have been logged out of your account. You are now in the signup menu.");
+    }
 
-    public static void showCurrentMenu() {}
+    public Result goProfileMenu() {
+        App.getApp().setCurrentMenu(Menu.PROFILE_MENU);
+        return new Result(true, "You are now in the profile menu.");
+    }
+
+    public Result goGameMenu() {
+        App.getApp().setCurrentMenu(Menu.GAME_MENU);
+        return new Result(true, "You are now in the game menu.");
+    }
+
+    public Result showCurrentMenu() {
+        return new Result(true, "You are now in the current menu.");
+    }
+
 }

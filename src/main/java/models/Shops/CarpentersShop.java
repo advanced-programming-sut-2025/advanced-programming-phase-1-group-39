e9de68857e8d2ea3f6c916f6f9a4255244782941
Abolfazl <abolfazlghasemi9388@gmail.com>
2025-05-10 10:59:08 +0330

    + add JojaMartShop and its json file

--- src/main/java/models/Shops/CarpentersShop.java
+++ src/main/java/models/Shops/CarpentersShop.java
@@ -16,8 +16,8 @@ public class CarpentersShop extends Shop{
     private HashMap<String, ShopItem> shopItems = new HashMap<>();
     private HashMap<String, BuildingData> buildings = new HashMap<>();
 
-    public CarpentersShop(String name, Location location, int width, int height, int openHour, int closeHour, NPC owner) {
-        super(name, location, width, height, openHour, closeHour, owner);
+    public CarpentersShop(String name, int openHour, int closeHour, NPC owner) {
+        super(name, new Location(0,0), 0, 0, openHour, closeHour, owner);
         loadFromJson("src/main/resources/data/CarpentersShop.json");
     }
 
--- src/main/java/models/Shops/JojaMartShop.java
+++ src/main/java/models/Shops/JojaMartShop.java
@@ -1,92 +1,68 @@
 package models.Shops;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import models.Constants;
 import models.Enums.Season;
 import models.Location;
 import models.NPC.NPC;
 import models.Result;
 
+import java.io.FileReader;
+import java.lang.reflect.Type;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class JojaMartShop extends Shop {
 
     private HashMap<String, ShopItem> permanentStock = new HashMap<>();
+    private Map<Season, HashMap<String, ShopItem>> seasonalStockBySeason = new HashMap<>();
     private HashMap<String, ShopItem> seasonalStock = new HashMap<>();
     private HashMap<String, Integer> dailyPurchases = new HashMap<>();
 
     private Season currentSeason;
 
     public JojaMartShop(String name, int openHour, int closeHour, NPC owner, Season currentSeason) {
-        super(name, new Location(0,0), 0, 0, openHour, closeHour, owner);
+        super(name, new Location(0, 0), 0, 0, openHour, closeHour, owner);
         this.currentSeason = currentSeason;
-
-        setupPermanentStock();
-        setupSeasonalStock(currentSeason);
+        loadFromJson("src/main/resources/data/JojaMartShop.json");
     }
 
-    private void setupPermanentStock() {
-        permanentStock.put("Joja Cola", new ShopItem("Joja Cola", 75, Constants.INFINITY));
-        permanentStock.put("Ancient Seed", new ShopItem("Ancient Seed", 500, 1));
-        permanentStock.put("Grass Starter", new ShopItem("Grass Starter", 125, Constants.INFINITY));
-        permanentStock.put("Sugar", new ShopItem("Sugar", 125, Constants.INFINITY));
-        permanentStock.put("Wheat Flour", new ShopItem("Wheat Flour", 125, Constants.INFINITY));
-        permanentStock.put("Rice", new ShopItem("Rice", 250, Constants.INFINITY));
-    }
+    public void loadFromJson(String path) {
+        try {
+            Gson gson = new Gson();
+            FileReader reader = new FileReader(path);
 
-    private void setupSeasonalStock(Season season) { // Todo: must be called but where?
-        seasonalStock.clear();
+            Type type = new TypeToken<JojaMartData>() {}.getType();
+            JojaMartData data = gson.fromJson(reader, type);
 
-        switch (season) {
-            case SPRING:
-                seasonalStock.put("Parsnip Seeds", new ShopItem("Parsnip Seeds", 25, 5));
-                seasonalStock.put("Bean Starter", new ShopItem("Bean Starter", 75, 5));
-                seasonalStock.put("Cauliflower Seeds", new ShopItem("Cauliflower Seeds", 100, 5));
-                seasonalStock.put("Potato Seeds", new ShopItem("Potato Seeds", 62, 5));
-                seasonalStock.put("Strawberry Seeds", new ShopItem("Strawberry Seeds", 100, 5));
-                seasonalStock.put("Tulip Bulb", new ShopItem("Tulip Bulb", 25, 5));
-                seasonalStock.put("Kale Seeds", new ShopItem("Kale Seeds", 87, 5));
-                seasonalStock.put("Coffee Beans", new ShopItem("Coffee Beans", 200, 1));
-                seasonalStock.put("Carrot Seeds", new ShopItem("Carrot Seeds", 5, 10));
-                seasonalStock.put("Rhubarb Seeds", new ShopItem("Rhubarb Seeds", 100, 5));
-                seasonalStock.put("Jazz Seeds", new ShopItem("Jazz Seeds", 37, 5));
-                break;
+            // Load permanent stock
+            for (ShopItemData item : data.permanentStock) {
+                permanentStock.put(item.name, new ShopItem(item.name, item.price, item.limit));
+            }
 
-            case SUMMER:
-                seasonalStock.put("Tomato Seeds", new ShopItem("Tomato Seeds", 62, 5));
-                seasonalStock.put("Pepper Seeds", new ShopItem("Pepper Seeds", 50, 5));
-                seasonalStock.put("Wheat Seeds", new ShopItem("Wheat Seeds", 12, 10));
-                seasonalStock.put("Summer Squash Seeds", new ShopItem("Summer Squash Seeds", 10, 10));
-                seasonalStock.put("Radish Seeds", new ShopItem("Radish Seeds", 50, 5));
-                seasonalStock.put("Melon Seeds", new ShopItem("Melon Seeds", 100, 5));
-                seasonalStock.put("Hops Starter", new ShopItem("Hops Starter", 75, 5));
-                seasonalStock.put("Poppy Seeds", new ShopItem("Poppy Seeds", 125, 5));
-                seasonalStock.put("Spangle Seeds", new ShopItem("Spangle Seeds", 62, 5));
-                seasonalStock.put("Starfruit Seeds", new ShopItem("Starfruit Seeds", 400, 5));
-                seasonalStock.put("Coffee Beans", new ShopItem("Coffee Beans", 200, 1));
-                seasonalStock.put("Sunflower Seeds", new ShopItem("Sunflower Seeds", 125, 5));
-                break;
+            // Load seasonal stock for all seasons
+            for (Map.Entry<String, List<ShopItemData>> entry : data.seasonalStock.entrySet()) {
+                Season season = Season.valueOf(entry.getKey());
+                HashMap<String, ShopItem> seasonMap = new HashMap<>();
+                for (ShopItemData item : entry.getValue()) {
+                    seasonMap.put(item.name, new ShopItem(item.name, item.price, item.limit));
+                }
+                seasonalStockBySeason.put(season, seasonMap);
+            }
 
-            case FALL:
-                seasonalStock.put("Corn Seeds", new ShopItem("Corn Seeds", 187, 5));
-                seasonalStock.put("Eggplant Seeds", new ShopItem("Eggplant Seeds", 25, 5));
-                seasonalStock.put("Pumpkin Seeds", new ShopItem("Pumpkin Seeds", 125, 5));
-                seasonalStock.put("Broccoli Seeds", new ShopItem("Broccoli Seeds", 15, 5));
-                seasonalStock.put("Amaranth Seeds", new ShopItem("Amaranth Seeds", 87, 5));
-                seasonalStock.put("Grape Starter", new ShopItem("Grape Starter", 75, 5));
-                seasonalStock.put("Beet Seeds", new ShopItem("Beet Seeds", 20, 5));
-                seasonalStock.put("Yam Seeds", new ShopItem("Yam Seeds", 75, 5));
-                seasonalStock.put("Bok Choy Seeds", new ShopItem("Bok Choy Seeds", 62, 5));
-                seasonalStock.put("Cranberry Seeds", new ShopItem("Cranberry Seeds", 300, 5));
-                seasonalStock.put("Sunflower Seeds", new ShopItem("Sunflower Seeds", 125, 5));
-                seasonalStock.put("Fairy Seeds", new ShopItem("Fairy Seeds", 250, 5));
-                seasonalStock.put("Rare Seed", new ShopItem("Rare Seed", 1000, 1));
-                seasonalStock.put("Wheat Seeds", new ShopItem("Wheat Seeds", 12, 5));
-                break;
+            // Set initial seasonal stock
+            setupSeasonalStock(currentSeason);
 
-            case WINTER:
-                seasonalStock.put("Powdermelon Seeds", new ShopItem("Powdermelon Seeds", 20, 10));
-                break;
+            reader.close();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+    }
+
+    private void setupSeasonalStock(Season season) {
+        seasonalStock = seasonalStockBySeason.getOrDefault(season, new HashMap<>());
     }
 
     public void changeSeason(Season newSeason) {
@@ -135,5 +111,15 @@ public class JojaMartShop extends Shop {
         // TODO: parse commands like: "buy Parsnip Seeds 3"
     }
 
-}
+    // Data holder classes for JSON parsing
+    public static class JojaMartData {
+        List<ShopItemData> permanentStock;
+        Map<String, List<ShopItemData>> seasonalStock;
+    }
 
+    public static class ShopItemData {
+        String name;
+        int price;
+        int limit;
+    }
+}

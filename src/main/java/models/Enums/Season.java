5f3555618c9a628ebb654f3fd1768fab0ecf8e9d
Abolfazl <abolfazlghasemi9388@gmail.com>
2025-05-11 16:59:08 +0330

    + update season and DayOfWeek and time from mireslami + add some methods to time

--- src/main/java/models/Enums/Season.java
+++ src/main/java/models/Enums/Season.java
@@ -1,13 +1,25 @@
 package models.Enums;
 
-import java.util.Objects;
-
 public enum Season {
-    SPRING,
-    SUMMER,
-    FALL,
-    WINTER;
+    SPRING (0),
+    SUMMER (1),
+    FALL (2),
+    WINTER (3);
 
+    public int number;
+
+    Season(int number) {
+        this.number = number;
+    }
+
+    public static Season getSeasonByNumber(int number) {
+        for (Season season : Season.values()) {
+            if (season.number == number) {
+                return season;
+            }
+        }
+        return null;
+    }
     public static Season getSeason(String name) {
         if (name.equalsIgnoreCase("spring")) {
             return SPRING;

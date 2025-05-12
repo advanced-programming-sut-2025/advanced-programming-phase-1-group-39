df2317c11913392ad5b15f1bdd17bc0dba6614df
mshahmahdi <mostafashahmahdi87@gmail.com>
2025-05-12 15:10:52 +0330

    fix some classes and add DialogueLoader

--- src/main/java/models/map/TileType.java
+++ src/main/java/models/map/TileType.java
@@ -16,12 +16,24 @@ public enum TileType {
     private final char symbol;
     private final boolean walkable;
 
+    private boolean canPlant = false;
+    private boolean isWatered = false;
+    private boolean isFertalized = false;
+
     TileType(char symbol, boolean walkable) {
         this.symbol = symbol;
         this.walkable = walkable;
     }
 
     public boolean isWalkable() { return walkable; }
+
+    public void setCanPlant() { canPlant = true; }
+    public void setIsFertalized() { isFertalized = true; }
+    public void setIsWatered() { isWatered = true; }
+
+    public boolean canPlant() { return canPlant; }
+    public boolean isFertalized() { return isFertalized; }
+    public boolean isWatered() { return isWatered; }
 
     public char getSymbol() { return symbol; }
 }

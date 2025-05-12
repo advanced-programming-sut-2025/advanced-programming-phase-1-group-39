fa59058a88602d3598e329695047cdc9fc4ce315
SeyyedHossein Mireslami <mireslami809@gmail.com>
2025-05-12 15:54:22 +0330

    artisan machine from abolfazl

--- src/main/java/models/artisan/ArtisanMachine.java
+++ src/main/java/models/artisan/ArtisanMachine.java
@@ -1,14 +1,24 @@
 package models.artisan;
 
+import models.Player;
+import models.Result;
+import models.Time;
+import models.crafting.CraftingItem;
+
 import java.util.ArrayList;
 
-public class ArtisanMachine {
-    private ArrayList<ArtisanRecipe> recipes;
+public abstract class ArtisanMachine extends CraftingItem {
+    protected ArrayList<ArtisanRecipe> recipes = new ArrayList<>();
 
-    private ArrayList<ArtisanGood> processingGoods;
-    private ArrayList<ArtisanGood> readyGoods;
-    private boolean isWorking;
+    protected ArtisanRecipe processingRecipe;
+    protected ArtisanGood readyGood;
+    protected boolean isWorking;
+    protected Time processTime;
 
-    public void use() {}
-    public ArrayList<ArtisanGood> getReadyGoods() {return null;}
+    public ArtisanMachine(String name, int sellPrice) {
+        super(name, sellPrice);
+    }
+
+    public Result use(String itemName, String ingredient, Time time, Player player) {return null;}
+    public ArtisanGood getReadyGoods(String name, Time time) {return null;}
 }
--- src/main/java/models/artisan/ArtisanMachineType.java
+++ src/main/java/models/artisan/ArtisanMachineType.java
@@ -1,21 +1,22 @@
 package models.artisan;
 
+import java.util.Map;
+
 public enum ArtisanMachineType {
-    BEE_HOUSE(new ArtisanMachine()),
-    CHEESE_PRESS(new ArtisanMachine()),
-    MAYO_MACHINE(new ArtisanMachine()),
-    KEG(new ArtisanMachine()),
-    PRESERVE_JAR(new ArtisanMachine()),
-    OIL_MAKER(new ArtisanMachine()),
-    LOOM(new ArtisanMachine()),
-    CHARCOAL_KILN(new ArtisanMachine()),
-    FURNACE(new ArtisanMachine()),
-    FISH_SMOKER(new ArtisanMachine()),
-    MUSHROOM_DRIER(new ArtisanMachine());
-
-    public final ArtisanMachine machine;
-    ArtisanMachineType(ArtisanMachine machine) {
-        this.machine = machine;
-    }
+    CHARCOAL_KILN(),
+    FURNACE,
+    BEE_HOUSE,
+    CHEESE_PRESS,
+    KEG,
+    LOOM,
+    MAYO_MACHINE,
+    OIL_MAKER,
+    PRESERVE_JAR,
+    DEHYDRATOR,
+    FISH_SMOKER;
+//
+//    private Map<String, ArtisanRecipe> recipes;
+//    ArtisanMachineType(Map<String, ArtisanRecipe> recipes) {
+//        this.recipes = recipes;
+//    }
 }
-

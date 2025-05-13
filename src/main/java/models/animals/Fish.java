bd3aee5e39dd4dfc77eefba51bc496fd5312dcc2
SeyyedHossein Mireslami <mireslami809@gmail.com>
2025-05-13 15:44:05 +0330

    fish changed

--- src/main/java/models/animals/Fish.java
+++ src/main/java/models/animals/Fish.java
@@ -1,17 +1,54 @@
 package models.animals;
 
 import models.Enums.Season;
+import models.Item;
 
-public class Fish {
-    int energy;
-    FishType type;
-    int price;
-    Season season;
+public class Fish extends Item {
+    private FishType type;
+    private int price;
+    private Season season;
+    private AnimalProductQuality quality;
 
-    public Fish(int energy, FishType type, int price, Season season) {
-        this.energy = energy;
+    public Fish(String name, FishType type, int price, Season season) {
+        super(name);
         this.type = type;
         this.price = price;
         this.season = season;
+        this.quality = AnimalProductQuality.NORMAL; // initial Todo:
+    }
+
+    public FishType getType() {
+        return type;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public Season getSeason() {
+        return season;
+    }
+
+    public AnimalProductQuality getQuality() {
+        return quality;
+    }
+
+    public void setQuality(AnimalProductQuality quality) {
+        this.quality = quality;
+    }
+
+    public int getFinalPrice() {
+        return (int) (price * quality.getRate());
+    }
+
+    @Override
+    public String toString() {
+        return "Fish{" +
+                "type=" + type +
+                ", price=" + price +
+                ", season=" + season +
+                ", quality=" + quality +
+                ", name='" + name + '\'' +
+                '}';
     }
 }

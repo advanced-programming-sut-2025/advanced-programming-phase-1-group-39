bab355a03d83349e0018a8fd44c7dd5928b23fc4
Abolfazl <abolfazlghasemi9388@gmail.com>
2025-05-13 15:59:25 +0330

    + add FertilizerType

---
+++ src/main/java/models/cropsAndFarming/FertilizerType.java
@@ -0,0 +1,18 @@
+package models.cropsAndFarming;
+
+public enum FertilizerType {
+    SPEED,
+    QUALITY;
+
+    public FertilizerType getType(String input) {
+        if (input.equalsIgnoreCase("Deluxe Retaining Soil") ||
+                input.equalsIgnoreCase("Basic Retaining Soil")
+                || input.equalsIgnoreCase("Quality Retaining Soil")) {
+            return QUALITY;
+        }
+        if (input.equalsIgnoreCase("Speed-Gro")) {
+            return SPEED;
+        }
+        return null;
+    }
+}

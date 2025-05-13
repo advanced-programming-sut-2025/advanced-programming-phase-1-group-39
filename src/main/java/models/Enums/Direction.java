15d959662a82eb831976ce590051a4186450ccc1
SeyyedHossein Mireslami <mireslami809@gmail.com>
2025-05-13 10:24:52 +0330

    directions completed

--- src/main/java/models/Enums/Direction.java
+++ src/main/java/models/Enums/Direction.java
@@ -1,5 +1,20 @@
 package models.Enums;
 
 public enum Direction {
-    UP, DOWN, LEFT, RIGHT, UPRIGHT, UPLEFT, DOWNRIGHT, DOWNLEFT;
+    UP(0, -1),
+    DOWN(0, 1),
+    LEFT(-1, 0),
+    RIGHT(1, 0),
+    UPRIGHT(1, -1),
+    UP_LEFT(-1, -1),
+    DOWNRIGHT(1, 1),
+    DOWN_LEFT(-1, 1);
+
+    public final int dx;
+    public final int dy;
+
+    Direction(int dx, int dy) {
+        this.dx = dx;
+        this.dy = dy;
+    }
 }

9142f747e3301174dbd2dd2b2fc0e13594d8db9d
mshahmahdi <mostafashahmahdi87@gmail.com>
2025-05-13 16:19:17 +0330

    add get reward function

--- src/main/java/models/NPC/AbigailNPC.java
+++ src/main/java/models/NPC/AbigailNPC.java
@@ -1,10 +1,11 @@
 package models.NPC;
 
-import models.ItemManager;
-import models.ItemStack;
-import models.Location;
+import models.*;
 import models.services.DialogueLoader;
 import models.services.MissionsLoader;
+import models.tools.Tool;
+import models.tools.ToolType;
+import models.tools.WateringCan;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,5 +30,39 @@ public class AbigailNPC extends NPC {
                 , new ItemStack(ItemManager.getItemByName("Wheat"), 50)));
         super.missions = MissionsLoader.loadMissionsFromJson("src/main/resources/data/Missions/abigailMissions.json");
         super.quests = generateNPCQuests(tasks, missions);
+    }
+
+    @Override
+    public void getRewardMission1(int friendShipLevel, Game game) {
+        Player currentPlayer = game.getCurrentPlayer();
+        currentPlayer.getInventory().pickItem(getQuest(1).getTask().getItem().getName(),
+                getQuest(1).getTask().getAmount());
+        if (friendShipLevel == 2) {
+            currentPlayer.getFriendship("abigail").setFriendshipLevel(3);
+        } else if (friendShipLevel == 1) {
+            currentPlayer.getFriendship("abigail").setFriendshipLevel(2);
+        }
+    }
+
+    @Override
+    public void getRewardMission2(int friendShipLevel, Game game) {
+        Player currentPlayer = game.getCurrentPlayer();
+        currentPlayer.getInventory().pickItem(getQuest(2).getTask().getItem().getName(),
+                getQuest(2).getTask().getAmount());
+        if (friendShipLevel == 2) {
+            currentPlayer.addMoney(1000);
+        } else {
+            currentPlayer.addMoney(500);
+        }
+    }
+
+    @Override
+    public void getRewardMission3(int friendShipLevel, Game game) {
+        Player currentPlayer = game.getCurrentPlayer();
+        currentPlayer.getInventory().pickItem(getQuest(3).getTask().getItem().getName(),
+                getQuest(3).getTask().getAmount());
+
+        WateringCan wateringCan = (WateringCan) currentPlayer.getInventory().getItemByName("watering can").getItem();
+        wateringCan.upgradeTo(ToolType.IRIDIUM);
     }
 }

cbde56b8daa3786140b082ad9cd9de6d03ab7ac4
Abolfazl <abolfazlghasemi9388@gmail.com>
2025-05-14 21:07:20 +0330

    + delete unnecessary method handle command from Shop

--- src/main/java/models/Shops/Shop.java
+++ src/main/java/models/Shops/Shop.java
@@ -22,7 +22,7 @@ public class Shop extends Building {
         this.owner = owner;
     }
 
-    public void handleCommand(String command){}
+    public Result purchase(String productName, int amount) {return null;}
     public String showAllProducts(){return null;}
     public String showAvailableProducts() {return null;}
     public void endDay() {};

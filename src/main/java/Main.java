26f283281b4d0f6269625e9dd2fede18ec0243e7
Abolfazl <abolfazlghasemi9388@gmail.com>
2025-05-15 11:09:10 +0330

    + some edit in Crop

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,32 @@
+import controllers.AppControllers;
+import models.App;
 import models.Enums.Season;
+import models.Enums.commands.GameCommands;
+import models.ItemManager;
+import models.Location;
+import models.NPC.NPC;
+import models.Player;
+import models.Shops.*;
+import models.artisan.*;
+import models.cooking.Food;
+import models.cooking.FoodRecipe;
+import models.crafting.CraftingManager;
+import models.crafting.CraftingRecipe;
 import models.cropsAndFarming.CropManager;
 import models.cropsAndFarming.ForagingManager;
 import models.cropsAndFarming.TreeManager;
 import models.map.Tile;
 
 import java.io.IOException;
+import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args){
-        TreeManager.loadTrees("src/main/java/models/cropsAndFarming/trees.json");
-        ForagingManager.loadTreeSeeds("src/main/java/models/cropsAndFarming/ForagingTrees.json");
-        ForagingManager.getRandomTree(Season.SPRING, new Tile(2, 3));
+        Shop newShop = new PierresGeneralStore("salam", new Location(0,0), 0, 0, "src/main/resources/data/PierresGeneralStore.json",
+                0, 0, new NPC("mmd"));
+        //System.out.println(newShop.showAllProducts());
+        //System.out.println(newShop.showAvailableProducts());
+        ItemManager.loadItems();
+        System.out.println(ItemManager.getItemByName("Vinegar").getName());
     }
 }

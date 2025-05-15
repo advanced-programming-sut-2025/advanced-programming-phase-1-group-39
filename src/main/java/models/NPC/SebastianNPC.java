a48da3893abc868732c63dfb66e6f12b0bf48f34
SeyyedHossein Mireslami <mireslami809@gmail.com>
2025-05-15 22:30:44 +0330

    added loadItems to loadApp

--- src/main/java/models/NPC/SebastianNPC.java
+++ src/main/java/models/NPC/SebastianNPC.java
@@ -16,8 +16,8 @@ public class SebastianNPC extends NPC {
                 "programmer",
                 "Sebastian is a brooding and introverted character who loves technology and enjoys spending time alone.\nHis hobbies include tinkering with gadgets and playing video games.\nHe has a soft spot for pizza and pumpkins, and although he seems distant at first, he appreciates meaningful connections over time.",
                 new Location(30, 15),
-                new ArrayList<>(List.of((ItemManager.getItemByName("pumpkin pie")), (ItemManager.getItemByName("Wool")),
-                        (ItemManager.getItemByName("pizza"))))
+                new ArrayList<>(List.of((ItemManager.getItemByName("Pumpkin Pie")), (ItemManager.getItemByName("Wool")),
+                        (ItemManager.getItemByName("Pizza"))))
 
         );
     }

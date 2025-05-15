1a395dd54e06e15c0c0ef50b249c555c176e33a8
SeyyedHossein Mireslami <mireslami809@gmail.com>
2025-05-15 07:28:21 +0330

    getting the fruit of tree and removing weeds by Scythe

--- src/main/java/models/tools/Scythe.java
+++ src/main/java/models/tools/Scythe.java
@@ -2,6 +2,7 @@ package models.tools;
 
 import models.*;
 import models.cropsAndFarming.Plant;
+import models.cropsAndFarming.Tree;
 import models.map.Tile;
 import models.map.TileType;
 
@@ -15,6 +16,10 @@ public class Scythe extends Tool {
         if (tile.getType() == TileType.SOIL) {
             Plant plant = tile.getPlant();
             if (plant != null) {
+                if (!plant.isAlive()) {
+                    tile.removePlant();
+                    return new Result(true, "You remove the died plants");
+                }
                 if (plant.hasCrop()) {
                     ItemStack product = plant.harvest();
                     if (!player.getInventory().hasSpace(product))
@@ -25,12 +30,20 @@ public class Scythe extends Tool {
                 else
                     return new Result(false, "Nothing to harvest.");
             }
-            if (tile.getItemOnTile().getItem().getName().equals("grass")) {
+            if (tile.getItemOnTile().getItem().getName().equals("Grass")) {
                 ItemStack item = tile.getItemOnTile();
                 if (!player.getInventory().hasSpace(item))
                     return new Result(false, "You don't have enough space to get objects!");
                 player.getInventory().addItem(item.getItem(), item.getAmount());
                 return new Result(true, "You cut the grasses!");
+            }
+            if (tile.getTree() != null) {
+                Tree tree = tile.getTree();
+                ItemStack fruits = tree.harvest();
+                if (!player.getInventory().hasSpace(fruits))
+                    return new Result(false, "You don't have enough space to get objects!");
+                player.getInventory().addItem(fruits.getItem(), fruits.getAmount());
+                return new Result(true, "You got fruit: " + fruits.getItem().getName());
             }
         }
         return new Result(false, "ِYou did nothing!");

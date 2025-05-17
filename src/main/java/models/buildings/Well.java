927d4ad2ba177e75d87021f22d1fc772d218da43
Abolfazl <abolfazlghasemi9388@gmail.com>
2025-05-17 09:31:50 +0330

    + handle build buildings in CarpentersShop + update updateMAp in Well

--- src/main/java/models/buildings/Well.java
+++ src/main/java/models/buildings/Well.java
@@ -1,9 +1,36 @@
 package models.buildings;
 
 import models.Location;
+import models.map.Map;
+import models.map.Tile;
+import models.map.TileType;
 
 public class Well extends Building {
     public Well(String name, Location location, int width, int height) {
         super(name, location, width, height);
+    }
+
+    @Override
+    public void updateMap(Map map) {
+        Tile[][] tiles = map.getTiles();
+        int x = this.getLocation().x();
+        int y = this.getLocation().y();
+        int w = this.getWidth();
+        int h = this.getHeight();
+
+        for (int i = x + 1; i < x + w - 1; x++) {
+            for (int j = y + 1; j < y + h - 1; j++) {
+                tiles[j][i].setType(TileType.WATER);
+            }
+        }
+
+        for (int i = x; i < x + w; i++) {
+            tiles[y][i].setType(TileType.WALL);
+            tiles[y + h - 1][i].setType(TileType.WALL);
+        }
+        for (int j = y ; j < y + h ; j++) {
+            tiles[j][x].setType(TileType.WALL);
+            tiles[j][x + w - 1].setType(TileType.WALL);
+        }
     }
 }
